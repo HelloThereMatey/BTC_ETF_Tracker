2d25d048faed248a6a12f414ebb875e6559fb483
Bootleg Macro Pleb <purpyfurcat@proton.me>
2024-05-27 15:00:55 +1000

    fff

--- BTC_ETF_Tracker.py
+++ BTC_ETF_Tracker.py
@@ -11,7 +11,7 @@ parent = os.path.dirname(wd)
 st.set_page_config(page_icon=":bird:")
 
 logo = plt.imread(wd+fdel+"Macro_Bootlegger.jpg")
-bitty = plt.imread(wd+fdel+"bitcoin.png", format="png")
+bitty = plt.imread(wd+fdel+"bitcoin.jpg")
 menu_items = {"Get help": "www.suckle_adingaling.com", "Report a bug": "www.suckle_adingaling.com", "About": "www.suckle_adingaling.com"}
 page_items = {"Flows_Dollars":"ETF daily in/outflow (USD)", "Flows_BTC":"ETF daily in/outflow (BTC)", "AUM_USD":"ETF AUM (USD)",
               "AUM_BTC":"ETF AUM (BTC)"}
