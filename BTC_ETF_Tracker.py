70d690c93cab9f42b26d54918cd87e259577ed3f
Bootleg Macro Pleb <purpyfurcat@proton.me>
2024-05-27 14:51:29 +1000

    hv

--- BTC_ETF_Tracker.py
+++ BTC_ETF_Tracker.py
@@ -11,13 +11,13 @@ parent = os.path.dirname(wd)
 st.set_page_config(page_icon=":bird:")
 
 logo = plt.imread(wd+fdel+"Macro_Bootlegger.jpg")
-bitty = plt.imread(wd+fdel+"bitcoin.png")
+bitty = plt.imread(wd+fdel+"bitcoin.png", format="png")
 menu_items = {"Get help": "www.suckle_adingaling.com", "Report a bug": "www.suckle_adingaling.com", "About": "www.suckle_adingaling.com"}
 page_items = {"Flows_Dollars":"ETF daily in/outflow (USD)", "Flows_BTC":"ETF daily in/outflow (BTC)", "AUM_USD":"ETF AUM (USD)",
               "AUM_BTC":"ETF AUM (BTC)"}
 
 st.title("Bitcoin: U.S Spot ETF Tracker")
-st.image(bitty, use_column_width=False, width = 150)
+st.image(bitty, use_column_width=False, width = 100)
 st.subheader("By the Macro Bootlegger.")
 block_url = "https://www.theblock.co/data/crypto-markets/bitcoin-etf"
 farside_url = "https://farside.co.uk/?p=997"
